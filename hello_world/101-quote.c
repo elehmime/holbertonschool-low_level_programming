e6300956eb737a435b6347377fd444f4189fb86f
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 <farouk.lehmime@gmail.com>
2022-09-29 18:26:17 -0700

    ex101

---
+++ hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+ * main - Main function
+ *
+ * Return: Exit status code
+ */
+
+int	main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 58);
+	return (1);
+}
