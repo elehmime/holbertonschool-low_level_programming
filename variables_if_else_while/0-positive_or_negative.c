12244efc9628fc2ca1dd902c90e7cf6edd85d53f
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 <farouk.lehmime@gmail.com>
2022-10-02 17:20:00 -0700

    ex00-3

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,13 @@
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h>
 /* more headers goes there */
-
+#include <stdio.h>
 /* betty style doc for function main goes there */
+/**
+* main - Main function
+*
+* Return: Exit status code
+*/
 int main(void)
 {
 	int n;
@@ -11,11 +15,6 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	/**
-	 *
-	 * if conditions
-	 *
-	 */
 	if (n > 0)
 		printf("%d is positive\n", n);
 	if (n < 0)
