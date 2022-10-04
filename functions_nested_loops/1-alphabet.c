4b6d1bea077bb17efacae2af2ee6b6e8c7565530
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 <farouk.lehmime@gmail.com>
2022-10-03 18:19:49 -0700

    ex01

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+/**
+ * print_alphabet - function
+ */
+void	print_alphabet(void)
+{
+	char	c;
+
+	c = 'a';
+	while (c < 'z')
+	{
+		write(1, &c, 1);
+		c++;
+	}
+	write(1, "\n", 1);
+}
