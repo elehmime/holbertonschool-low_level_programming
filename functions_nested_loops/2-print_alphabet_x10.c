2e9b99d0de88bad6403d9802856eaf028bd5bad0
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 <farouk.lehmime@gmail.com>
2022-10-03 18:29:47 -0700

    ex02

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+
+/**
+ * print_alphabet_x10 - function
+ */
+void print_alphabet_x10(void)
+{
+	char	c;
+	int	i;
+
+	c = 'a';
+	i = 0;
+	while (i <= 10)
+	{
+		while (c <= 'z')
+		{
+			write(1, &c, 1);
+			c++;
+		}
+		i++;
+	}
+}
