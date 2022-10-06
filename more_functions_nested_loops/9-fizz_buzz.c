dd842797fdb84893fb0bb5cc0abd2f5c7df068c1
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 <farouk.lehmime@gmail.com>
2022-10-05 23:29:41 -0700

    ex09

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - function
+ * Return: 0
+ */
+int	main(void)
+{
+	int	c = 1;
+
+	if (c < 100)
+	{
+		while (c < 100)
+		{
+			if (c % 5 == 0 && c % 3 == 0)
+			{
+				printf("fizzbuzz ");
+			}
+			else if (c % 5 == 0)
+			{
+				printf("buzz ");
+			}
+			else if (c % 3 == 0)
+			{
+				printf("fizz ");
+			}
+			else
+			{
+				printf("%d ", c);
+			}
+			c++;
+		}
+		printf("buzz\n");
+	}
+	return (0);
+}
