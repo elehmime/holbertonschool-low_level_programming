37fa8ebe749a51d81e242f2102cb839d5e24d3d2
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 <farouk.lehmime@gmail.com>
2022-10-05 22:18:20 -0700

    ex07

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ *print_diagonal - function
+ *@n: int
+ */
+void	print_diagonal(int n)
+{
+	int	c;
+	int	b;
+
+	c = 0;
+	b = 0;
+	if (n > 1)
+	{
+		while (c < n)
+		{
+			while (b < c)
+			{
+				_putchar('.');
+				b++;
+			}
+			_putchar('\\');
+			_putchar('\n');
+			c++;
+			b = 0;
+		}
+	}
+	_putchar('\n');
+}
