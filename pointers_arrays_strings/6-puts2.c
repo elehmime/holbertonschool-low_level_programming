33c5d9ed760f708ceea91fb8b85e257a974d53cf
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 <farouk.lehmime@gmail.com>
2022-10-09 22:46:27 -0700

    ex06-2

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -7,9 +7,14 @@
 void	puts2(char *str)
 {
 	int	i;
+	int	len;
 
 	i = 0;
-	while (str[i] != '\0')
+	len = 0;
+	while(str[i++])
+		len++;
+	i = 0;
+	while (i < len)
 	{
 	_putchar(str[i]);
 		i += 2;
