26308ff3887f412a4e567ef17708ec419183ae5b
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 <farouk.lehmime@gmail.com>
2022-10-09 22:55:10 -0700

    ex07

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *puts_half - function
+ *@str: char
+ *
+ */
+void	puts_half(char *str)
+{
+	int	len;
+	int	i;
+
+	len = 0;
+	i = 0;
+	while (str[i++])
+		len++;
+	i = 0;
+	while (i < len)
+	{
+		if (i >= len / 2)
+			_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
