32ab08fdabc2c9aa4ca911d4e91ab0fcaf0db71e
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 <farouk.lehmime@gmail.com>
2022-10-12 01:28:11 -0700

    ex100

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ *_atoi - function
+ *@s: char
+ *
+ *Return: value
+ */
+int	_atoi(char *s)
+{
+		int i;
+	int	n;
+	int	atoi;
+
+	i = 0;
+	atoi = 0;
+	while (s[i] && !(s[i] >= '0' && s[i] <= '9'))
+		i++;
+	n = 1;
+	while (s[i] == '-' || s[i] == '+')
+	{
+		if (s[i] == '-')
+			n = n * -1;
+		i++;
+	}
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		atoi = atoi * 10 + s[i] - 48;
+		i++;
+	}
+	return (n * atoi);
+}
