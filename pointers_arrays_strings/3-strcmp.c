4a1567b60b1bb808827779b364f016bd0c75a0db
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 <farouk.lehmime@gmail.com>
2022-10-13 02:21:06 -0700

    ex03-2strcmp

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -8,18 +8,11 @@
  */
 int	_strcmp(char *s1, char *s2)
 {
-	int	i = 0;
-	int	j = 0;
+	while (*s1 && *s2 && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
 
-	while (s1[i])
-		i++;
-	while (s2[j])
-		j++;
-	if (i == j)
-		return (0);
-	if (i > j)
-		return (15);
-	if (i < j)
-		return (-15);
-	return (0);
+	return (*s1 - *s2);
 }
