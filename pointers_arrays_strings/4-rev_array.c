8d9fdada64a79d23a6d3054d9a04db875c8a4f56
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 <farouk.lehmime@gmail.com>
2022-10-13 03:02:48 -0700

    ex04-reverse_array

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *reverse_array - function
+ *@a: int
+ *@n: int
+ *
+ */
+void	reverse_array(int *a, int n)
+{
+	int i = 0;
+	int   t = 0;
+
+	while (i <= n / 2)
+	{
+		t = a[i];
+		a[i] = a[n - 1 - i];
+		a[n - 1 - i] = t;
+		i++;
+	}
+}
