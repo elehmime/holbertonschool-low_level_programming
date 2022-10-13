2bd39becff980c47c438f2a66fb80a46598e106d
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 <farouk.lehmime@gmail.com>
2022-10-13 08:12:57 -0700

    ex07-leet

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -7,20 +7,25 @@
  */
 char	*leet(char *str)
 {
-	int i = 0;
+	int	i = 0;
+	int	y = 0;
+	char	*a;
+	char	*b;
 
+	a = "aAeEoOtTlL";
+	b = "4433007711";
 	while (str[i])
 	{
-		if (str[i] == 'a' || str[i] == 'A')
-			str[i] = '4';
-		if (str[i] == 'e' || str[i] == 'E')
-			str[i] = '3';
-		if (str[i] == 'o' || str[i] == 'O')
-			str[i] = '0';
-		if (str[i] == 't' || str[i] == 'T')
-			str[i] = '7';
-		if (str[i] == 'l' || str[i] == 'L')
-			str[i] = '1';
+		while (a[y])
+		{
+			if (str[i] == a[y])
+			{
+				str[i] = b[y];
+				break;
+			}
+			y++;
+		}
+		y = 0;
 		i++;
 	}
 	return (str);
--- pointers_arrays_strings/main.c
+++ pointers_arrays_strings/main.c
@@ -1,42 +1,18 @@
+#include "main.h"
 #include <stdio.h>
 
-void reverse_array(int *, int);
-
 /**
- * print_array - Print an array of int
- *
- * @a: an array of integers
- * @n: the number of elements in @a
- */
-void print_array(int *a, int n)
-{
-	int i;
-
-	i = 0;
-	while (i < n)
-	{
-		if (i != 0)
-			printf(", ");
-		printf("%d", a[i]);
-		i++;
-	}
-	printf("\n");
-}
-
-/**
- * main - check the code for Holberton School students.
+ * main - check the code for
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	int a[] = {
-		234234, 64534, 77632, 8723451, 98987, 1345231, 98534597,
-		23456789, 6235456, 41235416, 374578, 10435434, 89734567
-	};
+    char s[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\n";
+    char *p;
 
-	print_array(a, sizeof(a) / sizeof(int));
-	reverse_array(a, 0);
-	print_array(a, sizeof(a) / sizeof(int));
-	return (0);
+    p = leet(s);
+    printf("%s", p);
+    printf("%s", s);
+    return (0);
 }
